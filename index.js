e9ad118ec9e972699b0a6293446bd8c6fcefe1b7
Murtaza34 <murtaza.mohebi@gmail.com>
2024-01-16 22:57:59 +0000

    added a start button

--- index.js
+++ index.js
@@ -3,66 +3,44 @@ $(document).ready(function () {
   const red_btn = $("#red");
   const blue_btn = $("#blue");
   const yellow_btn = $("#yellow");
+  const yellow_sound = new Audio("/sounds/yellow.mp3");
+  // const green_sound = new Audio("/sounds/green.mp3");
+  const blue_sound = new Audio("/sounds/blue.mp3");
+  const red_sound = new Audio("/sounds/red.mp3");
 
-  const sequence = [];
+
+  const computer_sequence = [];
   const user_sequence = [];
+  const button_colors = ["green", "red", "yellow", "blue"];
+  let game_started = false;
   let game_over = false;
   let score = 0;
   let high_score = 0;
 
   // -------------- Main game loop -------------- //
-  $(document).click(function () {});
-
-  // get random number between 1 - 4
-  function generate_sequence() {
-    const randomNumber = Math.floor(Math.random() * 4) + 1;
-    console.log(randomNumber);
-    if (randomNumber === 1) {
-      sequence.push("green");
-    } else if (randomNumber === 2) {
-      sequence.push("red");
-    } else if (randomNumber === 3) {
-      sequence.push("blue");
-    } else if (randomNumber === 4) {
-      sequence.push("yellow");
+  $(document).on("click tap", function () {
+    if (!game_started) {
+      // $(".start-message").text("Score ");
+      // $("#display-score").text(score)
+      // generate_sequence();
     }
+  });
+
+  // get random number between 0 - 4
+  function generate_sequence() {
+    const randomNumber = Math.floor(Math.random() * button_colors.length);
+    const randomColor = button_colors[randomNumber];
+    computer_sequence.push(randomColor);
+    if (randomColor === "green") {
+      green_btn.fadeOut(100).fadeIn(80);
+      // const green_sound = new Audio("/sounds/green.mp3");
+      // green_sound.play();
+    }
+    console.log(computer_sequence);
   }
-  generate_sequence();
+  // generate_sequence();
 
   function display_sequence() {}
   function get_user_input() {}
   function check_sequence() {}
 });
-
-/*
-Initialize game:
-sequence = empty array
-user_sequence = empty array
-game_over = false
-
-Function generate_sequence(length):
-For i from 1 to length:
-Add random color to sequence
-
-Function display_sequence():
-For each color in sequence:
-Display color (light and sound)
-Wait for a short period
-
-Function get_user_input():
-Wait for user input (button press or mouse click)
-Add the pressed color to user_sequence
-
-Function check_sequence():
-If user_sequence is not equal to sequence:
-Set game_over to true
-
-Main game loop:
-While not game_over:
-generate_sequence(increase difficulty over time)
-display_sequence()
-get_user_input()
-check_sequence()
-
-Display "Game Over! Your score: {length of sequence - 1}"
-*/
