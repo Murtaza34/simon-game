4a2647c08bc5dbcf2903385966b0d8d43a9d77e7
Murtaza34 <murtaza.mohebi@gmail.com>
2024-01-17 10:59:29 +0000

    added both computer and user sequence

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
   const blue_sound = new Audio("/sounds/blue.mp3");
   const red_sound = new Audio("/sounds/red.mp3");
 
-
   const computer_sequence = [];
   const user_sequence = [];
   const button_colors = ["green", "red", "yellow", "blue"];
@@ -18,29 +17,39 @@ $(document).ready(function () {
   let high_score = 0;
 
   // -------------- Main game loop -------------- //
-  $(document).on("click tap", function () {
+  $(document).on("click tap", "#start-button", function () {
     if (!game_started) {
-      // $(".start-message").text("Score ");
-      // $("#display-score").text(score)
-      // generate_sequence();
+      $(".start-button").css("visibility", "hidden");
+      setTimeout(() => {
+        $("#display-score").text(`SCORE: ${score}`);
+        $("#display-score").css("visibility", "visible");
+      }, 500);
+      generate_sequence();
+      game_started = true;
     }
   });
 
-  // get random number between 0 - 4
   function generate_sequence() {
+    // get random number between 0 - 4
     const randomNumber = Math.floor(Math.random() * button_colors.length);
     const randomColor = button_colors[randomNumber];
     computer_sequence.push(randomColor);
-    if (randomColor === "green") {
-      green_btn.fadeOut(100).fadeIn(80);
-      // const green_sound = new Audio("/sounds/green.mp3");
-      // green_sound.play();
-    }
-    console.log(computer_sequence);
+    // setout a delay so user can see the order once start is pressed
+    setTimeout(() => {
+      $(`#${randomColor}`).fadeOut(100).fadeIn(80);
+      const computer_color_sound = new Audio(`/sounds/${randomColor}.mp3`);
+      computer_color_sound.play();
+    }, 500);
   }
-  // generate_sequence();
 
-  function display_sequence() {}
+  $(".btn").on('click', function(){
+    const userChosenColor = this.id
+    user_sequence.push(userChosenColor)
+    const user_color_sound = new Audio(`/sounds/${userChosenColor}.mp3`)
+    user_color_sound.play();
+    // console.log(user_sequence);
+  });
+
   function get_user_input() {}
   function check_sequence() {}
 });
